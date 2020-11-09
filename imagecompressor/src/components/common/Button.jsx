135d33e10000fc67820d0188f82ce1085df53076
michalstachecki <michal.stachecki@gmail.com>
2020-11-09 22:10:32 +0100

    Basic functionality refactoring

--- imagecompressor/src/components/common/Button.jsx
+++ imagecompressor/src/components/common/Button.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Button.css";
 
-class Button extends React.PureComponent {
-    render() {
-        const { click, hidden, label } = this.props;
+const Button = (props) => {
+    const { click, hidden, label } = props;
 
-        return(
-            <button className="app-button" onClick={click} hidden={hidden}>{label}</button>
-        );
-    }
+    return(
+        <button className="app-button" onClick={click} hidden={hidden}>{label}</button>
+    );
 }
 
 export default Button;
